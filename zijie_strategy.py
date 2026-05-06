c704547ee475257e079bdeef47c2c764b825c3d6
HiggRn <zijie.gu0115@gmail.com>
2026-05-05 22:05:36 -0400

    no need for static method

--- zijie_strategy.py
+++ zijie_strategy.py
@@ -22,37 +22,37 @@ class Strategy(BaseStrategy):
             row["MidPrice"] - self.history[0]["MidPrice"]
         ) / self.history[0]["MidPrice"]
 
-        return self.should_execute(
+        return should_execute(
             row, self.history, self.side, self.daily_return, self.tick_size
         )
 
-    @staticmethod
-    def should_execute(current, history, side, daily_trend, tick_size) -> bool:
-        # 1. Feature extraction
-        imbalance = (current["BidSize_1"] - current["AskSize_1"]) / (
-            current["BidSize_1"] + current["AskSize_1"]
-        )
-        spread = current["AskPrice_1"] - current["BidPrice_1"]
-        is_large_tick = spread <= 1.01 * tick_size
 
-        # 2. Price momentum
-        mid_change = current["MidPrice"] - history[-2]["MidPrice"]
+def should_execute(current, history, side, daily_trend, tick_size) -> bool:
+    # 1. Feature extraction
+    imbalance = (current["BidSize_1"] - current["AskSize_1"]) / (
+        current["BidSize_1"] + current["AskSize_1"]
+    )
+    spread = current["AskPrice_1"] - current["BidPrice_1"]
+    is_large_tick = spread <= 1.01 * tick_size
 
-        # 3. Global trend adjustment
-        trend_adj = np.clip(daily_trend * 10, -0.2, 0.2)
+    # 2. Price momentum
+    mid_change = current["MidPrice"] - history[-2]["MidPrice"]
 
-        if side == "BUY":
-            # - Large Tick: imbalance is high
-            # - Small Tick: mid price starts to fall
-            if is_large_tick:
-                return imbalance > (0.6 + trend_adj)
-            else:
-                return mid_change < 0 and imbalance > (0.2 + trend_adj)
+    # 3. Global trend adjustment
+    trend_adj = np.clip(daily_trend * 10, -0.2, 0.2)
 
-        else:  # SIDE == "SELL"
-            # - Large Tick: imbalance is low
-            # - Small Tick: mid price starts to rise
-            if is_large_tick:
-                return imbalance < (-0.6 + trend_adj)
-            else:
-                return mid_change > 0 and imbalance < (-0.2 + trend_adj)
+    if side == "BUY":
+        # - Large Tick: imbalance is high
+        # - Small Tick: mid price starts to fall
+        if is_large_tick:
+            return imbalance > (0.6 + trend_adj)
+        else:
+            return mid_change < 0 and imbalance > (0.2 + trend_adj)
+
+    else:  # SIDE == "SELL"
+        # - Large Tick: imbalance is low
+        # - Small Tick: mid price starts to rise
+        if is_large_tick:
+            return imbalance < (-0.6 + trend_adj)
+        else:
+            return mid_change > 0 and imbalance < (-0.2 + trend_adj)
